ee2f8bef3c82e3470a78da3e2d48d2fd018416d1
vahdettinA <vahdettin.21aydin@gmail.com>
2025-10-25 11:55:58 +0300

    add color file

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary
+  static const primary = Color(0xFF4CAF50);
+  static const primaryLight = Color(0xFFA5D6A7);
+  static const primaryDark = Color(0xFF388E3C);
+
+  // Secondary / Accent
+  static const secondary = Color(0xFFFFC107);
+  static const secondaryLight = Color(0xFFFFD54F);
+  static const secondaryDark = Color(0xFFFFA000);
+
+  // Backgrounds
+  static const background = Color(0xFFF5F5F5);
+  static const cardBackground = Color(0xFFFFFFFF);
+  static const backgroundDark = Color(0xFFE8F5E9);
+
+  // Text
+  static const textPrimary = Color(0xFF212121);
+  static const textSecondary = Color(0xFF757575);
+  static const textOnPrimary = Color(0xFFFFFFFF);
+
+  // Buttons
+  static const buttonPrimary = primary;
+  static const buttonSecondary = secondary;
+  static const buttonDisabled = Color(0xFFBDBDBD);
+
+  // Shadows
+  static const shadowLight = Color.fromRGBO(0, 0, 0, 0.1);
+  static const shadowMedium = Color.fromRGBO(0, 0, 0, 0.2);
+  static const shadowDark = Color.fromRGBO(0, 0, 0, 0.3);
+
+  // Alerts
+  static const error = Color(0xFFE53935);
+  static const success = Color(0xFF43A047);
+  static const info = Color(0xFF1E88E5);
+}
