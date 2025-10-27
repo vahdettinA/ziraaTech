c07c7bc78fa544dd144fc4254ed91fd24505d2ac
vahdettinA <vahdettin.21aydin@gmail.com>
2025-10-27 15:33:10 +0300

    farmer ve dealer color paletleri ayarlandı

--- lib/core/constants/colors.dart
+++ lib/core/constants/colors.dart
@@ -1,23 +1,28 @@
-import 'package:flutter/material.dart';
+import 'dart:ui';
 
 class AppColors {
-  // Ana Renk
-  static const Color primary = Color(0xFF2A9C8E);
+  // 🌿 Farmer Theme Colors
+  static const Color farmerPrimary = Color(0xFF299C8D);
+  static const Color farmerPrimaryDark = Color(0xFF1E766C);
+  static const Color farmerPrimaryLight = Color(0xFF5EC3B4);
+  static const Color farmerSecondary = Color(0xFFD9A441);
+  static const Color farmerBackground = Color(0xFFF5F7F6);
+  static const Color farmerSurface = Color(0xFFFFFFFF);
+  static const Color farmerTextPrimary = Color(0xFF1E1E1E);
+  static const Color farmerTextSecondary = Color(0xFF5A5A5A);
+  static const Color farmerSuccess = Color(0xFF4CAF50);
+  static const Color farmerWarning = Color(0xFFFFC107);
+  static const Color farmerError = Color(0xFFE53935);
 
-  // Açık Tonlar
-  static const Color primaryLight = Color(0xFF66BFB2);
-  static const Color primaryLighter = Color(0xFFA0D9CC);
-
-  // Koyu Tonlar
-  static const Color primaryDark = Color(0xFF1F6F63);
-  static const Color primaryDarker = Color(0xFF185047);
-
-  // Accent / Vurgu Renkleri
-  static const Color accentYellow = Color(0xFFF4D35E);
-  static const Color accentOrange = Color(0xFFEE6C4D);
-
-  // Nötr Renkler
-  static const Color background = Color(0xFFF8F8F8);
-  static const Color divider = Color(0xFFE0E0E0);
-  static const Color text = Color(0xFF333333);
+  // 💼 Dealer Theme Colors
+  static const Color dealerPrimary = Color(0xFF2D3E50);
+  static const Color dealerSecondary = Color(0xFF299C8D);
+  static const Color dealerAccent = Color(0xFFF4B400);
+  static const Color dealerBackground = Color(0xFFF7F9FA);
+  static const Color dealerSurface = Color(0xFFFFFFFF);
+  static const Color dealerTextPrimary = Color(0xFF1E1E1E);
+  static const Color dealerTextSecondary = Color(0xFF555555);
+  static const Color dealerSuccess = Color(0xFF4CAF50);
+  static const Color dealerWarning = Color(0xFFFB8C00);
+  static const Color dealerError = Color(0xFFE53935);
 }
